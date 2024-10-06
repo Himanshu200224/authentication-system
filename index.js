41ee1710c6457f0caabbb895df3ff8c7efbed161
Himanshu200224 <work.himanshukaushik@gmail.com>
2024-10-06 16:30:33 +0530

    did some changes in index.js
    applied middleware for validation when user will enter his/her details in the form then validation will run.

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-import User from "./mongooseSchema.js";
-import connectDb from "./mongodbConnection.js";
 import express from "express";
 import path from "path";
-import {fileURLToPath} from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const app=express();
-const port=3000;
-app.use(express.urlencoded({extended:true}));
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import connectDb from "./mongodbConnection.js"
+import User from "./mongooseSchema.js"
+import {__dirname} from "./utils.js"
+import { validateUserData} from "./validation.js";
+const app = express();
+const port = 3000;
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());//middle wares to accespt data from request.body
-const request=connectDb();//connecting to database here
-app.get('/',(req,res)=>{
-    res.send('connection success');
-    console.log('success');
-    
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended:false}));
+
+const request = connectDb();//connecting to database here
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/login.html"));
 });
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,"login.html"));
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/registration.html'));
 });
-app.get('/register',function(req,res){
-    res.sendFile(path.join(__dirname,'registration.html'));
+app.post('/submit',validateUserData,async function(req,res){
+    try{
+        const {username,email,password,}=req.body;
+    const user=new User({username,email,password});
+    await user.save();
+    res.json({ success: true, message: 'Data saved successfully' });
+    }catch(err){
+        res.status(400).json({ success: false, message: 'Failed to save data' });
+    }
 })
-app.get('/login/register',function(req,res){
-    
-})
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log("success");
+});
+request.then(()=>{
+    console.log("connection ban gaya");
+    
 })
